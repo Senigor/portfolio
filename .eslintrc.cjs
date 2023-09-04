fe73a97a46960f72e7d8134cc1f8731c41771730
Igor Dudchenko <igor.dudchenko@wpp.com>
2023-09-04 18:38:09 +0100

    Implement about, hero, loader, navbar components; create canvas with 3d computer object; add missing threejs library; modify eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,23 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:@react-three/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    // react-three-fiber issue https://github.com/pmndrs/react-three-fiber/discussions/2487,
+    // remove after plugin:@react-three/recommended handles this
+    "react/no-unknown-property": ["off", { ignore: ["JSX"] }],
   },
-}
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Lorem ipsum",
     icon: mobile,
   },
   {
@@ -54,7 +54,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Sit amet",
     icon: creator,
   },
 ];
