5c3a01041016649c04cfb93142ecc695e58b43a6
Senigor <dudchenko.igor15@gmail.com>
2023-09-04 23:10:04 +0100

    Animate stars particles; implement emails contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,8 +16,44 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs
+      .send(
+        "service_mzwhuuz",
+        "template_b6gejy7",
+        {
+          from_name: form.name,
+          to_name: "Igor",
+          from_email: form.email,
+          to_email: "dudchenko.igor15@gmail.com",
+          message: form.message,
+        },
+        "oOgaHxtviYwwV5F8F"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div
