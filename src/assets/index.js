a5188459c345c4d5d1fe57b8dfbf15a0306b67e5
Senigor <dudchenko.igor15@gmail.com>
2023-09-08 00:38:53 +0100

    Modify hero and experience sections, add more tech balls

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,24 +7,23 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
+import python from "./tech/python.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import fastapi from "./tech/fastapi.png";
+import django from "./tech/django.png";
+import graphql from "./tech/graphql.png";
+import aws from "./tech/aws.png";
+import gcp from "./tech/gcp.png";
+import postgresql from "./tech/postgresql.png";
+import elasticsearch from "./tech/elasticsearch.png";
+import redis from "./tech/redis.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import robin from "./company/robin.png";
+import star from "./company/star.png";
+import indevlab from "./company/indevlab.png";
 
 import clothingstore from "./clothingstore.png";
 import jobit from "./jobit.png";
@@ -39,23 +38,22 @@ export {
   github,
   menu,
   close,
-  css,
   docker,
-  figma,
   git,
-  html,
-  javascript,
   mongodb,
-  nodejs,
   reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  fastapi,
+  django,
+  graphql,
+  aws,
+  gcp,
+  postgresql,
+  elasticsearch,
+  redis,
+  python,
+  robin,
+  star,
+  indevlab,
   clothingstore,
   jobit,
   tripguide,
